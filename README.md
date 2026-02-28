587504a82072f24b6fddb7cc6569ad7f5fde826e
Xeena2812 <39200884+Xeena2812@users.noreply.github.com>
2026-02-28 17:49:04 +0900

    init projects

---
+++ README.md
@@ -0,0 +1,1 @@
+# Photo as a Service
---
+++ backend/PhotoAsAService.Api/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
